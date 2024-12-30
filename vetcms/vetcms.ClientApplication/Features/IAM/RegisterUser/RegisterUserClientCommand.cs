008dd5509725b969c99c81291f10c47673d42495
rizzilla <141221167+rizzilla@users.noreply.github.com>
2024-12-30 19:42:02 +0100

    Register user

---
+++ vetcms/vetcms.ClientApplication/Features/IAM/RegisterUser/RegisterUserClientCommand.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.ClientApplication.Common.Abstract;
+
+namespace vetcms.ClientApplication.Features.IAM.RegisterUser
+{
+    public class RegisterUserClientCommand : IClientCommand<bool>
+    {
+        public RegisterUserClientCommand(IDialogService _dialogService) : base(_dialogService)
+        {
+        }
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Password { get; set; }
+        public string Password2 { get; set; }
+    }
+}
