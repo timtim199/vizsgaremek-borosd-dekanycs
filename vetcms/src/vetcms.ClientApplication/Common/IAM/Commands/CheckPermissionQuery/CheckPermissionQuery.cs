6545b1bd6cf5ff45987cbf88d0386a338b5b323c
timtim <52172968+timtim199@users.noreply.github.com>
2025-01-20 18:13:19 +0100

    create CheckPermissionQuery

---
+++ vetcms/src/vetcms.ClientApplication/Common/IAM/Commands/CheckPermissionQuery/CheckPermissionQuery.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.FluentUI.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using vetcms.ClientApplication.Common.Abstract;
+using vetcms.ClientApplication.Common.IAM.Commands.AuthenticationStatus;
+using vetcms.SharedModels.Common.IAM.Authorization;
+
+namespace vetcms.ClientApplication.Common.IAM.Commands.CheckPermissionQuery
+{
+    public class CheckPermissionQuery : IClientCommand<bool>
+    {
+        public PermissionFlags[] RequestedPermissions { get; set; }
+
+        public CheckPermissionQuery(params PermissionFlags[] permissions)
+            : base()
+        {
+            RequestedPermissions = permissions;
+        }
+    }
+}
