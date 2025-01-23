2fa2dadda9a39836f92d5b54bc9a4b0f4dabab5b
timtim <52172968+timtim199@users.noreply.github.com>
2025-01-23 16:54:13 +0000

    wip

--- vetcms/src/vetcms.BrowserPresentation/Program.cs
+++ vetcms/src/vetcms.BrowserPresentation/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using vetcms.BrowserPresentation;
+using vetcms.BrowserPresentation.Services;
 
 internal class Program
 {
@@ -16,6 +17,7 @@ internal class Program
 
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+        builder.Services.AddScoped(typeof(ToastBuilderService));
         builder.Services.AddFluentUIComponents();
         builder.Services.AddDependencies();
         await builder.Build().RunAsync();
